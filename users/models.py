f13f83afdd6ae498945a10dcb2b345329265d6b9
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-12-04 20:45:06 +0300

    Создание приложений users и habits

---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="email")
+    avatar = models.ImageField(
+        upload_to="users/avatars", verbose_name="Аватар", **NULLABLE
+    )
+    tg_chat_id = models.CharField(
+        max_length=100, verbose_name="Телеграм чат-id", **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"

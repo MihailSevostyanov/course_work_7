6dd29bf3dfefbfe986e34101ebef91b44fe1b6d4
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-12-06 21:27:14 +0300

    Решение ошибки запуска сервера

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("NAME"),
         "PASSWORD": os.getenv("PASSWORD"),
-        "USER": os.getenv("USER"),
+        "USER": os.getenv("USER_DB"),
         "HOST": os.getenv("HOST"),
         "PORT": os.getenv("PORT"),
     }
